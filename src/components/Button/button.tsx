4e512e3063e85f2cd941ca282d74a7770ad17b64
Your Name <you@example.com>
2024-12-14 20:04:31 +0500

    handleClick done

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -2,6 +2,19 @@ type ButtonProps = {
   children: React.ReactNode;
 };
 const Button = ({ children }: ButtonProps) => {
-  return <button>{children}</button>;
+  const handleClick = () => {
+    alert("Работает");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      // onClick={() => {
+      //   alert("Работает");
+      // }}
+    >
+      {children}
+    </button>
+  );
 };
 export { Button };
